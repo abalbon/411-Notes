b259beea839a6388732f4d88aa3b94aa42035b0d
Albert Balbon <abalbon@csu.fullerton.edu>
2019-10-15 17:14:05 -0700

    Oct 15 Notes

--- app/src/main/java/com/csuf/cpsc41101/testapplication/PersonDetailActivity.java
+++ app/src/main/java/com/csuf/cpsc41101/testapplication/PersonDetailActivity.java
@@ -1,11 +1,19 @@
 package com.csuf.cpsc41101.testapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class PersonDetailActivity extends AppCompatActivity {
+
+    protected Menu detailMenu;
+    protected int personIndx;
+    protected Person pObj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,15 @@ public class PersonDetailActivity extends AppCompatActivity {
         //
         Person pObj = PersonDB.getInstance().getPersonList().get(personIndx);
 
+        // Make the edit text as read only
+        EditText editView = findViewById(R.id.p_first_name_id);
+        editView.setText(pObj.getFirstName());
+        editView.setEnabled(false);
+        editView = findViewById(R.id.p_last_name_id);
+        editView.setText(pObj.getLastName());
+        editView.setEnabled(false);
+
+
         // How to display the information - detailed view of each person obj
         /*
         Linear Layout:
@@ -32,5 +49,42 @@ public class PersonDetailActivity extends AppCompatActivity {
                 Make, Model, Year
             Button to add Vehicles    xmlns:app="http://schemas.android.com/apk/res-auto"
          */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Custom Menu inflation
+        getMenuInflater().inflate(R.menu.detail_screen_menu, menu);
+        menu.findItem(R.id.action_edit).setVisible(true);
+        menu.findItem(R.id.action_done).setVisible(false);
+        detailMenu = menu;
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // On Edit press, it will let the first and last name editable
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_edit) {
+            EditText editView = findViewById(R.id.p_first_name_id);
+            editView.setEnabled(true);
+            editView = findViewById(R.id.p_last_name_id);
+            editView.setEnabled(true);
+            // Make menu items visible/invisible based on which button is pressed
+            item.setVisible(false);
+            detailMenu.findItem(R.id.action_done).setVisible(true);
+        } else if(item.getItemId() == R.id.action_done) {
+            EditText editView = findViewById(R.id.p_first_name_id);
+            pObj.setFirstName(editView.getText().toString());
+            editView.setEnabled(false);
+            editView = findViewById(R.id.p_last_name_id);
+            pObj.setLastName(editView.getText().toString());
+            editView.setEnabled(false);
+            item.setVisible(false);
+            detailMenu.findItem(R.id.action_edit).setVisible(true);
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
